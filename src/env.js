222ffd67176c0061e46098b111e3c514d38f8fdf
carmelofndev <carmelofn.dev@gmail.com>
2024-06-12 22:09:00 +0200

    env variables v4

--- src/env.js
+++ src/env.js
@@ -1,6 +1,7 @@
 export const env = {
-    SERVER_URL: process.env.VITE_SERVER_URL,
-    SERVER_S3: process.env.VITE_SERVER_S3,
-    GOOGLE_MAPS_KEY: process.env.VITE_GOOGLE_MAPS_KEY,
-    RECAPTCHA_KEY: process.env.VITE_RECAPTCHA_KEY
+    SERVER_URL: import.meta.env.VITE_SERVER_URL,
+    SERVER_S3: import.meta.env.VITE_SERVER_S3,
+    GOOGLE_MAPS_KEY: import.meta.env.VITE_GOOGLE_MAPS_KEY,
+    RECAPTCHA_KEY: import.meta.env.VITE_RECAPTCHA_KEY
   };
+  
