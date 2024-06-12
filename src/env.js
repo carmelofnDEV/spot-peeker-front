701cc80517f9e53fb25f5e90d7eb26194f4b9985
carmelofndev <carmelofn.dev@gmail.com>
2024-06-12 22:05:38 +0200

    env variables v3

--- src/env.js
+++ src/env.js
@@ -1,6 +1,6 @@
 export const env = {
-    SERVER_URL: process.env.REACT_APP_SERVER_URL,
-    SERVER_S3: process.env.REACT_APP_SERVER_S3,
-    GOOGLE_MAPS_KEY: process.env.REACT_APP_GOOGLE_MAPS_KEY,
-    RECAPTCHA_KEY: process.env.REACT_APP_RECAPTCHA_KEY
+    SERVER_URL: process.env.VITE_SERVER_URL,
+    SERVER_S3: process.env.VITE_SERVER_S3,
+    GOOGLE_MAPS_KEY: process.env.VITE_GOOGLE_MAPS_KEY,
+    RECAPTCHA_KEY: process.env.VITE_RECAPTCHA_KEY
   };
